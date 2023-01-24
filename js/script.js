29d1ca8069c2509859b33fb4e13ac1670317775e
smallrussian <julian.shniter@gmail.com>
2023-01-24 13:27:01 -0600

    made gif search work

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 
 $(document).on('submit', function(event){
     event.preventDefault()
+    $("#results").empty()
     let number = Number($('#numberOfSearch').val())
     let input = $("#searchTerm").val()
     // alert(input)
@@ -8,7 +9,10 @@ $(document).on('submit', function(event){
         url: `https://api.giphy.com/v1/gifs/search?api_key=keYipRkymMnFbSPxWruiVnygeIrzibb0&q=${input}&limit=${number}&offset=0&rating=g&lang=en`,
         success: function(data){
             data.data.forEach(gif => {
-                console.log(gif)
+    
+                console.log(gif.images)
+                // $("#results").append(`<img src ="${gif.images.original.webp}"/>`)
+                $("#results").append(`<img src ="${gif.images.fixed_height.webp}"/>`)
             });
         } 
     })
