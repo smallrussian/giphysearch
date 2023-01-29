f5ceb4751e61ab5430e79445059625e8a0e262eb
smallrussian <julian.shniter@gmail.com>
2023-01-29 15:17:48 -0600

    fixed header and added fonts

--- js/script.js
+++ js/script.js
@@ -9,10 +9,7 @@ $(document).on('submit', function(event){
         url: `https://api.giphy.com/v1/gifs/search?api_key=keYipRkymMnFbSPxWruiVnygeIrzibb0&q=${input}&limit=${number}&offset=0&rating=g&lang=en`,
         success: function(data){
             data.data.forEach(gif => {
-    
                 console.log(gif.images)
-                // $("#results").append(`<img src ="${gif.images.original.webp}"/>`)
-                // $("#results > div.row").append(`<img src ="${gif.images.fixed_height.webp}"/>`)
                 $("#results > div.row").append(`<div class="image col-12 col-lg-3 col-md-4 col-sm-5 d-flex justify-content-center"><a href="${gif.images.original.url}"><img src="${gif.images.fixed_width.webp}"/></a></div>`);
             });
         } 
